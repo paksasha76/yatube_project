71a623ca86b83620bf19cc0c367504756b037d22
paksasha76 <pak.aleksandr2001@mail.ru>
2021-10-21 19:14:19 +0300

    Добавлены urls и viev-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+def index(request):    
+    return render('Главная страница')
+def group_posts(request):
+    return render('Группы')
 # Create your views here.
