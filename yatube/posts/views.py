78b062866a9f4cdf07efe9765c6b1ccf2304c617
paksasha76 <pak.aleksandr2001@mail.ru>
2021-10-22 11:06:32 +0300

    lddkkddk

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 def index(request):    
     return render('Главная страница')
-def group_posts(request):
-    return render('Группы')
+def group_posts(request, slug):
+    return render(f'Группы {slug}')
 # Create your views here.
