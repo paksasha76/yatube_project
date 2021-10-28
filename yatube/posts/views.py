175e66ddf7ac5a8b54b0c6ba53742056803024d1
paksasha76 <pak.aleksandr2001@mail.ru>
2021-10-28 19:06:20 +0300

    project

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
-    template = 'posts/index.html'    
-    return render(request, template)
+    template = 'posts/index.html' 
+    title = "Это главная страница ютуб"
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
+
 def group_posts(request, slug):
-    return HttpResponse(f'Группы {slug}')
+    template = 'posts/group_list.html'
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'text': text,
+    }
+    return render(request, {slug}, template, context)
 # Create your views here.
